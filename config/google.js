8e40ea00db2213220b2d5699c5f9f3f66c3a2455
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2016-05-29 23:07:42 +0200

    fix: remove configuration

--- config/google.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var expressConfig = require('./config').expressConfig;
-
-module.exports = {
-  'appID' : '569322145752-o6e4ssu69kbh55da0pab3g2fj8eum4bq.apps.googleusercontent.com',
-  'appSecret' : 'lBBdOLjqNxdrgNdi19aZ19Ov',
-  'callbackUrl' : 'http://' + expressConfig.server + ':' + expressConfig.port + '/api/auth/google/callback'
-};
